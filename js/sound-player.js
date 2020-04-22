86b697b5a7fdac1d382c0fe6a394e4062c534cd9
Osvaldo Contreras Jr <osvaldo@osvaldos-mbp.lan>
2020-04-22 08:36:48 -0500

    Resolved comments from the PR

--- js/sound-player.js
+++ js/sound-player.js
@@ -4,10 +4,10 @@ const soundItems = document.querySelectorAll('.sounds__item');
 // Function that enables/disables a specific sound when it has been clicked
 function handleSoundItemsClick(event) {
   const soundItem = event.currentTarget;
-  const slider = soundItem.querySelector('.slidecontainer');
+  const slider = soundItem.querySelector('.slide-container');
 
-  soundItem.classList.toggle('sounds__item-active');
-  slider.classList.toggle('slidecontainer-enabled');
+  soundItem.classList.toggle('sounds__item--active');
+  slider.classList.toggle('slide-container--enabled');
 }
 
 soundItems.forEach(item => item.addEventListener('click', handleSoundItemsClick));
