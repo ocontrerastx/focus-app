3b2ae7934750f8181573f0e5b0688ed44afdbfb5
Osvaldo Contreras Jr <osvaldo@osvaldos-mbp.lan>
2020-04-23 22:26:18 -0500

    Play Pause buttons now toggle

--- js/sound-player.js
+++ js/sound-player.js
@@ -1,4 +1,5 @@
-const playerButton = document.querySelector('.player__button');
+const playButton = document.querySelector('.player__button--play');
+const pauseButton = document.querySelector('.player__button--pause');
 const soundItems = document.querySelectorAll('.sounds__item');
 
 // Function that enables/disables a specific sound when it has been clicked
@@ -11,3 +12,24 @@ function handleSoundItemsClick(event) {
 }
 
 soundItems.forEach(item => item.addEventListener('click', handleSoundItemsClick));
+
+function handlePlayButtonClick(event) {
+  const clickedButton = event.currentTarget;
+
+  clickedButton.classList.toggle('player__button--disabled');
+  pauseButton.classList.toggle('player__button--disabled');
+
+  // Play Audio when button is clicked
+}
+
+function handlePauseButtonClick(event) {
+  const clickedButton = event.currentTarget;
+
+  clickedButton.classList.toggle('player__button--disabled');
+  playButton.classList.toggle('player__button--disabled');
+
+  // Pause Audio when button is clicked
+}
+
+playButton.addEventListener('click', handlePlayButtonClick);
+pauseButton.addEventListener('click', handlePauseButtonClick);
